7158242d667f762e48831a20c48b8a1756ac9d14
Princewill Iroka <princewilliroka@outlook.com>
2019-11-22 19:34:15 +0100

    Updates

---
+++ precache-manifest.1a6b0bfa0182bd0458231002a62ded23.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8b423fb703b23c070016ecc403e9ee6",
+    "url": "/PrincewillIroka.github.io/teamwork-frontend/index.html"
+  },
+  {
+    "revision": "ee3a76992d8f1fa93973",
+    "url": "/PrincewillIroka.github.io/teamwork-frontend/static/css/2.1f156bdb.chunk.css"
+  },
+  {
+    "revision": "956d0b0a1724e4334dfd",
+    "url": "/PrincewillIroka.github.io/teamwork-frontend/static/css/main.afde9bf1.chunk.css"
+  },
+  {
+    "revision": "ee3a76992d8f1fa93973",
+    "url": "/PrincewillIroka.github.io/teamwork-frontend/static/js/2.13deab80.chunk.js"
+  },
+  {
+    "revision": "956d0b0a1724e4334dfd",
+    "url": "/PrincewillIroka.github.io/teamwork-frontend/static/js/main.7c63c6dd.chunk.js"
+  },
+  {
+    "revision": "cbb8ebc287f8bb00b926",
+    "url": "/PrincewillIroka.github.io/teamwork-frontend/static/js/runtime-main.9de3f621.js"
+  },
+  {
+    "revision": "3680027d79bf412546943c74ea8c0af0",
+    "url": "/PrincewillIroka.github.io/teamwork-frontend/static/media/logo.3680027d.png"
+  },
+  {
+    "revision": "dbc96d1a038efb529687b53991b977ef",
+    "url": "/PrincewillIroka.github.io/teamwork-frontend/static/media/undraw_interaction_design_odgc.dbc96d1a.svg"
+  }
+]);
